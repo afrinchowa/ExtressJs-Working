0f09611069319d1f9b371a691684107ab282702e
chowa <arfinchowa524@gmail.com>
2024-11-16 00:01:16 +0600

    post man working

---
+++ dist/app/app.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = 3000;
+// parsers
+app.use(express_1.default.json());
+app.get('/', (req, res) => {
+    res.send('Hello developers,here is  chowa,what about you');
+});
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.json({
+        message: "successfully got data"
+    });
+});
+exports.default = app;
